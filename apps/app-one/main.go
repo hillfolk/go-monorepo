0fce8e9a10fcdb9ef89f29c96a3a94adf995925a
JunYong <pjy1418@gmail.com>
2023-01-14 19:59:52 +0900

    add: add to sample codes

--- apps/app-one/main.go
+++ apps/app-one/main.go
@@ -10,14 +10,13 @@ func main() {
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-
 		return c.String(http.StatusOK, "Hello, World! App One")
 	})
 	e.GET("/users", func(c echo.Context) error {
 		userList := []user.User{
 			{
 				Id:        1,
-				Email:     "user1@mai.com",
+				Email:     "user1@email.com",
 				Password:  "password",
 				FirstName: "john",
 				LastName:  "cena",
@@ -25,14 +24,17 @@ func main() {
 			},
 			{
 				Id:        2,
-				Email:     "user2@mai.com",
+				Email:     "user2@email.com",
 				Password:  "password",
 				FirstName: "john2",
 				LastName:  "cena2",
 				Phone:     "+821012345678",
 			},
 		}
-
+		userList = append(userList, *user.NewUser(3, "user3@email.com", "password", "john3", "cena3", "+821012345678"))
+		for i := 0; i < len(userList); i++ {
+			println(userList[i].ToString())
+		}
 		return c.JSON(http.StatusOK, userList)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
--- apps/app-two/main.go
+++ apps/app-two/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/hillfolk/go-monorepo/domain/orders"
 	echo "github.com/labstack/echo/v4"
+	zap "go.uber.org/zap"
 	"net/http"
 )
 
@@ -26,6 +27,19 @@ func main() {
 				ProductId: 2,
 				Quantity:  2,
 			},
+		}
+		orderList = append(orderList, *orders.NewOrder(3, 3, 3, 3, 3))
+		for i := 0; i < len(orderList); i++ {
+			logger, _ := zap.NewProduction()
+			defer logger.Sync()
+			logger.Info("failed to fetch URL",
+				// Structured context as strongly typed Field values.
+				zap.Int("orderId", int(orderList[i].Id)),
+				zap.Int("userId", int(orderList[i].UserId)),
+				zap.Int("productId", int(orderList[i].ProductId)),
+				zap.Int("quantity", int(orderList[i].Quantity)),
+				zap.String("orderTime", orderList[i].OrderTime.Format("2006-01-02 15:04:05")),
+			)
 		}
 		return c.JSON(http.StatusOK, orderList)
 	})
